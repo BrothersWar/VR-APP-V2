e53a2a4d62af6b5721490cc20efdd1280a1337f1
BrothersWar <alex.xd.2011@yandex.ru>
2020-12-08 23:36:42 +0300

    переделал отображение результата рана, поменял музыку, запаковал апк, игра ВПРИНЦИПЕ готова

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public GameObject GameOverPanel;
+    public GameObject ScoreInGame;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            GameOverPanel.SetActive(true);
+            ScoreInGame.SetActive(false);
+            Time.timeScale = 0;
+        }
+
+
+    }
+}
